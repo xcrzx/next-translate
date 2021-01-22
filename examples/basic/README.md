61e1b740f4f8bd001ad3953d6405945eaa645c83
Dmitry Shevchenko <dmshevch@gmail.com>
2021-01-22 10:32:25 +0100

    Rm examples & clean step

--- examples/basic/README.md
+++
@@ -1,3 +0,0 @@
-# Basic example
-
-![next-translate](../../images/translation-prerendered.gif 'Translations in prerendered pages')
--- examples/complex/next.config.js
+++
@@ -1,9 +0,0 @@
-const nextTranslate = require('next-translate')
-const withMDX = require('@next/mdx')()
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-
-console.log('Webpack version', require('webpack').version)
-
-module.exports = nextTranslate(withBundleAnalyzer(withMDX()))
